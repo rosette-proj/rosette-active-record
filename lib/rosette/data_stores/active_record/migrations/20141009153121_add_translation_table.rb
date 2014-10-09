f1a58de8a0a52eab29836ec3616840999b5edef7
Matt Low <mlow@lumoslabs.com>
2014-10-09 15:32:32 -0700

    Use ActiveRecord migrator and rename migrations

---
+++ lib/rosette/data_stores/active_record/migrations/20141009153121_add_translation_table.rb
@@ -0,0 +1,18 @@
+# encoding: UTF-8
+
+class AddTranslationTable < ActiveRecord::Migration
+  def up
+    create_table :translations do |t|
+      t.references :phrase
+      t.string :locale, limit: 10
+      t.text :translation
+      t.timestamps
+    end
+
+    add_index :translations, [:phrase_id, :locale, :translation], length: { translation: 255 }
+  end
+
+  def down
+    drop_table :translations
+  end
+end
--- lib/rosette/data_stores/active_record/tasks/schema_manager.rb
+++ lib/rosette/data_stores/active_record/tasks/schema_manager.rb
@@ -1,5 +1,7 @@
 # encoding: UTF-8
 
+require 'active_record/migration'
+
 class SchemaManager
 
   class SchemaMigration < ActiveRecord::Base
@@ -15,41 +17,17 @@ class SchemaManager
     end
 
     def migrate
-      each_pending_migration do |version, migration_class|
-        ActiveRecord::Base.transaction do
-          migration_class.new.up
-          SchemaMigration.create(version: version)
-        end
-      end
+      ActiveRecord::Migrator.migrate(migration_files_path)
     end
 
     def rollback
-      if record = SchemaMigration.last
-        previous_migration_classes = data_store_constants
-        file = path_from(record.version)
-
-        load file
-        migration_class_diff = data_store_constants - previous_migration_classes
-
-        if migration_class = migration_class_diff.first
-          migration_const = data_store_namespace.const_get(migration_class)
-
-          ActiveRecord::Base.transaction do
-            migration_const.new.down
-            record.destroy
-          end
-        end
-      end
+      ActiveRecord::Migrator.rollback(migration_files_path)
     end
 
     private
 
     def migration_files_path
       File.expand_path('../../migrations', __FILE__)
-    end
-
-    def migration_files
-      Dir.glob(File.join(migration_files_path, '**/*.rb'))
     end
 
     def connection
@@ -67,56 +45,5 @@ class SchemaManager
 
       connection.add_index(:schema_migrations, :version, unique: true)
     end
-
-    def file_versions
-      migration_files.map do |file_name|
-        version_from(file_name)
-      end
-    end
-
-    def version_from(file_name)
-      File.basename(file_name).gsub(/\.rb\z/, '')
-    end
-
-    def file_from(version)
-      "#{version}.rb"
-    end
-
-    def path_from(version)
-      File.join(migration_files_path, file_from(version))
-    end
-
-    def completed_versions
-      SchemaMigration.pluck(:version)
-    end
-
-    def pending_migrations
-      file_versions - completed_versions
-    end
-
-    def data_store_namespace
-      Rosette::DataStores::ActiveRecordDataStore
-    end
-
-    def data_store_constants
-      data_store_namespace.constants
-    end
-
-    def each_pending_migration
-      previous_migration_classes = data_store_constants
-
-      pending_migrations.sort.each do |version|
-        file = path_from(version)
-        load file
-        migration_class_diff = data_store_constants - previous_migration_classes
-
-        if migration_class = migration_class_diff.first
-          yield version, data_store_namespace.const_get(migration_class)
-        end
-
-        previous_migration_classes = data_store_constants
-      end
-    end
-
   end
 end
