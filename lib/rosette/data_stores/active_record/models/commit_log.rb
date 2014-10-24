94ee1608d7e1410e0fdecd9f91555be0df26520e
Cameron Dutro <camertron@gmail.com>
2014-10-23 23:51:38 -0700

    Adjust foreign keys in CommitLog and CommitLogLocale

--- lib/rosette/data_stores/active_record/models/commit_log.rb
+++ lib/rosette/data_stores/active_record/models/commit_log.rb
@@ -10,6 +10,8 @@ module Rosette
         validates :commit_id, presence: true
         validates :status, inclusion: { in: STATUSES }
 
+        has_many :commit_log_locales, foreign_key: :commit_id, primary_key: :commit_id
+
       end
     end
   end
--- lib/rosette/data_stores/active_record/models/commit_log_locale.rb
+++ lib/rosette/data_stores/active_record/models/commit_log_locale.rb
@@ -1,8 +1,11 @@
+# encoding: UTF-8
+
 module Rosette
   module DataStores
     class ActiveRecordDataStore
       class CommitLogLocale < ActiveRecord::Base
-        belongs_to :commit_log, foreign_key: :commit_id
+
+        belongs_to :commit_log, foreign_key: :commit_id, primary_key: :commit_id
 
         validates :commit_id, presence: true
         validates :locale, presence: true
