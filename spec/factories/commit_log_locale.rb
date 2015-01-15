95495f95c2c00907b0426d73f35ae595942aeddd
Matt Low <mlow@lumoslabs.com>
2015-01-15 14:47:25 -0800

    Add CommitLogLocale factory

---
+++ spec/factories/commit_log_locale.rb
@@ -0,0 +1,9 @@
+# encoding: UTF-8
+
+FactoryGirl.define do
+  factory :commit_log_locale, class: Rosette::DataStores::ActiveRecordDataStore::CommitLogLocale do
+    sequence :commit_id, 'aaaa'
+    sequence :locale, 'aa-AA'
+    # commit_log
+  end
+end
