814ff7537f5c0d404139278c1035223afd7a55a6
Matt Low <mlow@lumoslabs.com>
2015-01-15 15:12:54 -0800

    Removed commented code

--- spec/factories/commit_log_locale.rb
+++ spec/factories/commit_log_locale.rb
@@ -4,6 +4,5 @@ FactoryGirl.define do
   factory :commit_log_locale, class: Rosette::DataStores::ActiveRecordDataStore::CommitLogLocale do
     sequence :commit_id, 'aaaa'
     sequence :locale, 'aa-AA'
-    # commit_log
   end
 end
